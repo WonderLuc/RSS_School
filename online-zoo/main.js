a441ae1f900a449084a77bef6f243d01f8484295
wonderLuc <workmaildo@yandex.ru>
2021-04-05 20:31:01 +0500

    feat: add Feed block

--- online-zoo/main.js
+++ online-zoo/main.js
@@ -6,5 +6,10 @@ function goToMap(){
   window.location.assign("./pages/map/map.html");
 }
 
+function goToDonate(){
+  window.location.assign("./pages/donate/donate.html");
+}
+
 document.querySelector('.watch .btn').addEventListener('click', goToZoos);
 document.querySelector('.zoos .btn').addEventListener('click', goToMap);
+document.querySelector('.feed .btn').addEventListener('click', goToDonate);
