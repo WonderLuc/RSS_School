56058cdd0fed7d4dcb1e3d9858c2612de4712ab5
wonderLuc <workmaildo@yandex.ru>
2021-04-07 12:51:54 +0500

    feat: navigation to animal pages from slider

--- online-zoo/main.js
+++ online-zoo/main.js
@@ -1,3 +1,6 @@
+//temporary array with animal pages. He MUST be removed when pages will have appears
+const pages = ['panda', 'gorilla', 'eagle', 'alligator'];
+
 function goToZoos(){
   window.location.assign("./pages/zoos/panda.html");
 }
@@ -14,8 +17,27 @@ function scrollToHeader(e){
   window.scrollTo(0,0);
 }
 
+function goToAnimal(e){
+  let elem = e.target;
+  while(!elem.classList.contains('animals')){
+    if(elem.classList.contains('animal')){
+      break;
+    }
+    elem = elem.parentElement;
+  }
+
+  if(elem.classList.contains('animals')){
+    return;
+  }
+
+  let pageName = elem.classList.toString().match(/animal_(.+)/)[1];
+  //REMOVE WITH PAGES-ARRAY
+  return pages.includes(pageName)? window.location.assign(`./pages/zoos/${pageName}.html`) : console.log('404');
+  
+}
 
 document.querySelector('.watch .btn').addEventListener('click', goToZoos);
 document.querySelector('.zoos .btn').addEventListener('click', goToMap);
 document.querySelector('.contacts .btn').addEventListener('click', goToDonate);
 document.querySelector('footer .logo').addEventListener('click', scrollToHeader);
+document.querySelector('.animals').addEventListener('click', goToAnimal);
