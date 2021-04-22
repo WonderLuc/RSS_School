c6a101f4c8c8d2915903d352d57add950ee32ae8
wonderLuc <workmaildo@yandex.ru>
2021-04-22 12:30:42 +0500

    feat: add filters behaviour

--- photo-filter/index.js
+++ photo-filter/index.js
@@ -1,4 +1,4 @@
-//Fullscreen behaviour
+// Fullscreen behaviour
 let fullscreenBtn = document.querySelector('.fullscreen');
 
 fullscreenBtn.addEventListener('click', () => {
@@ -8,3 +8,25 @@ fullscreenBtn.addEventListener('click', () => {
   }
   document.exitFullscreen();
 });
+
+// Filters behaviour 
+let filtersContainer = document.querySelector('.filters');
+const measure = {
+  'blur' : 'px',
+  'invert' : '%',
+  'sepia' : '%',
+  'saturate' : '%',
+  'hue' : 'deg'
+}
+
+function updateValue (event) {
+  const elem = event.target;
+  const output = elem.nextElementSibling;
+  const name = elem.getAttribute('name');
+  output.innerText = elem.value;
+  document.documentElement.style.setProperty(`--${name}`, `${elem.value}${measure[name]}`);
+}
+
+[].forEach.call(filtersContainer.children, function (elem) {
+  elem.addEventListener('input', updateValue);
+} )
