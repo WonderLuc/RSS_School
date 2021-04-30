3648825db6c5098be1cbc7d8eeabbda8ff71dcf1
wonderLuc <workmaildo@yandex.ru>
2021-04-30 20:16:13 +0500

    feat: add testimonial slider

--- online-zoo/main.js
+++ online-zoo/main.js
@@ -37,6 +37,7 @@ function goToAnimal(e){
   
 }
 
+// Scroll for Pets-slider
 function scrollPetsSlider(isForward = true){
   let animalsContainer = document.querySelector('.animals');
   if ((animalsContainer.scrollLeft === 0 && !isForward) ||
@@ -49,6 +50,109 @@ function scrollPetsSlider(isForward = true){
   let x = isForward? animalsContainer.clientWidth: - animalsContainer.clientWidth;
   animalsContainer.scrollBy(x,0);
 }
+
+let wrapper =  document.querySelector('.testimonials');
+let testimonial = document.querySelector('.testimonial-wrapper');
+let widthCalc = Math.floor(wrapper.clientWidth / testimonial.clientWidth);
+let heightCalc = Math.floor(wrapper.clientHeight / testimonial.clientHeight);
+
+//Testimonials behaviour
+function renderTestimonials(){  
+  let viewElements = (widthCalc > heightCalc? widthCalc : heightCalc) + 7;
+  while (wrapper.children.length != viewElements) {
+    let newTestimonial = document.createElement('div');
+    newTestimonial.classList.add('testimonial-wrapper');
+    newTestimonial.innerHTML = `
+    <div class="testimonial">
+    <div class="testimonial__about">
+      <img class="avatar" src="./assets/images/ava-${Math.round(Math.random()*2 + 2)}.png" alt="avatar">
+      <div class="description">
+        <h4 class="description__name">User_${Math.round(Math.random()*10000)}</h4>
+        <p class="description__place">Local Austria</p>
+        <p class="description__time">Week Ago</p>
+      </div>
+    </div>
+    <p class="testimonial__text">My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My son delighted very much ljves to watch gouillas.
+      The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.                
+    </p>
+    </div>
+    `;
+    wrapper.append(newTestimonial);
+  }
+}
+renderTestimonials();
+
+let interval = setInterval(scrollTestimonials, 10000);
+let offset = 1;
+let isEnd = false;
+
+function scrollTestimonials(){
+  if(isEnd){
+    wrapper.scrollTo(0,0);
+    offset = 1;
+    document.querySelector('.progress-bar__current').style.left = `0%`;
+    isEnd= false;
+    return;
+  }
+  widthCalc > heightCalc ? wrapper.scrollBy(testimonial.clientWidth + 30,0) :
+                           wrapper.scrollBy(0, testimonial.clientHeight + 15);
+  offset++;
+  let percent = (offset * 100)/wrapper.children.length;
+  document.querySelector('.progress-bar__current').style.left = `${percent}%`;
+  if ((wrapper.scrollWidth - (wrapper.clientWidth + wrapper.scrollLeft)) === 0) {
+    isEnd = true;
+  }
+}
+
+
+// Stop for testimonial
+document.querySelector('.testimonials').addEventListener('click', (e) => {
+  let elem = e.target;
+  while (!elem.classList.contains('testimonial-wrapper')) {
+    if (elem.classList.contains('testimonials')) {
+      return;
+    }
+    elem = elem.parentElement;
+  }
+  clearInterval(interval);
+  console.log('Start 40sec pause');
+  setTimeout( () => {
+    console.log('continue slide every 10sec');
+    scrollTestimonials();
+    interval = setInterval(scrollTestimonials, 10000);
+  }, 40000)
+});
+
+// progress-bar move
+let progress = document.querySelector('.progress-bar__current');
+function moveProgressBar(event){
+  let progressContainer = document.querySelector('.progress-bar');
+  if (progressContainer.getBoundingClientRect().x >= progress.getBoundingClientRect().x ) {
+    progress.style.left = '1px';
+    return ;
+  }
+  if ((progressContainer.getBoundingClientRect().x + progressContainer.getBoundingClientRect().width) <=
+     (progress.getBoundingClientRect().x + progress.getBoundingClientRect().width)) {
+    progress.style.left = '81%';
+    return ;
+  }
+  let position = event.pageX - progressContainer.getBoundingClientRect().x - progress.getBoundingClientRect().width/3;
+  progress.style.left = `${position}px`;
+  let currentPercent = Math.round((100*position)/progressContainer.getBoundingClientRect().width);
+  if ( currentPercent%10 === 0) {
+    offset = currentPercent/10;
+    wrapper.scrollTo(offset*(testimonial.getBoundingClientRect().width +30), 0);
+  }
+}
+document.querySelector('.progress-bar__current').ondragstart = () => {return false};
+document.querySelector('.progress-bar__current').addEventListener('mousedown', (e) => {
+  clearInterval(interval);
+  document.addEventListener('mousemove', moveProgressBar);
+});
+document.addEventListener('mouseup', (e) => {
+  document.removeEventListener('mousemove', moveProgressBar);
+  interval = setInterval(scrollTestimonials, 10000);
+});
 
 document.querySelector('.watch .btn').addEventListener('click', goToZoos);
 document.querySelector('.zoos .btn').addEventListener('click', goToMap);
