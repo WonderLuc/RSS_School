483c5552e09405415545f87975d8a273611f10a4
wonderLuc <workmaildo@yandex.ru>
2021-07-05 13:49:58 +0500

    feat: Add Router

--- english-for-kids/src/components/App/App.tsx
+++ english-for-kids/src/components/App/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import {
+  BrowserRouter, Switch, Route, Link,
+} from 'react-router-dom';
+import Category from '../Categoty/Category';
+import Main from '../Main/Main';
+import Header from '../Header/Header';
 
 export default function App(): JSX.Element {
-  const changeColor = (e: React.MouseEvent<HTMLHeadingElement, MouseEvent>) => {
-    e.currentTarget.style.color = 'red';
-  };
-
   return (
-    <div className="app">
-      <h1 onClick={changeColor}>React Kids</h1>
+    <div className='app'>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path='/'><Main/></Route>
+          <Route path='/category'><Category /></Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
