587ef081ad011f1514a820de5fd238f2c60e31ee
wonderLuc <workmaildo@yandex.ru>
2021-07-07 20:22:22 +0500

    refactor: change target for handlers

--- english-for-kids/src/components/Main/Main.tsx
+++ english-for-kids/src/components/Main/Main.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useTypedSelector } from '../../hooks/useTypedSelector';
-import { dataEnd, getData } from '../../store/actions';
+import { closeMenu, dataEnd, getData } from '../../store/actions';
 import CategoryCard from '../CategotyCard/CategoryCard';
 import Download from '../Download/Download';
 import PlaySwitcher from '../PlaySwitcher/PlaySwitcher';
 import './style.scss';
 
 function Main(): JSX.Element {
-  const { dataManageReducer: dataState } = useTypedSelector((store) => store);
+  const { dataManageReducer: dataState, menuManageReducer: menuState } = useTypedSelector((store) => store);
   const dispatch = useDispatch();
+  function closeOpenMenu() {
+    if (menuState.isOpenNav) {
+      dispatch(closeMenu());
+    }
+  }
   const changeColor = (e: React.MouseEvent<HTMLHeadingElement, MouseEvent>) => {
     const bol = dataState.loading ? dispatch(dataEnd()) : dispatch(getData());
     e.currentTarget.style.color = dataState.loading ? 'black' : 'red';
   };
 
   return (
-    <main className="main">
+    <main className="main" onClick={closeOpenMenu}>
       <PlaySwitcher />
       {dataState.loading && <Download />}
       <h2 onClick={changeColor}>Main Page</h2>
--- english-for-kids/src/components/Navigation/Navigation.tsx
+++ english-for-kids/src/components/Navigation/Navigation.tsx
@@ -11,12 +11,12 @@ export default function Navigation(props: NavProps): JSX.Element {
   const { dataManageReducer: dataState, playModeManageReducer: playState } = useTypedSelector((store) => store);
   return (
     <ul className={`nav ${playState.isPlay ? 'nav_play' : ''}`}>
-      <li className="nav__item" onClick={(e) => props.onClick(e)}><NavLink exact to='/'>Main</NavLink></li>
+      <li className="nav__item" ><NavLink exact to='/' onClick={(e) => props.onClick(e)}>Main</NavLink></li>
       {dataState.categories.map((category) => {
         const key = `header-${category.name}`;
         return (
-          <li className="nav__item" key={key} onClick={(e) => props.onClick(e)}>
-            <NavLink to={`/category/${category.name}`} >{category.name}</NavLink>
+          <li className="nav__item" key={key} >
+            <NavLink onClick={(e) => props.onClick(e)} to={`/category/${category.name}`} >{category.name}</NavLink>
           </li>);
       })}
     </ul>
